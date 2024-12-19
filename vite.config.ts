26a0ccc152ae512a46b1faa5327bc0f8fcb503ae
Aryan <aryanpandita003@gmail.com>
2024-12-19 22:35:57 +0530

    31th

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      ethers5: 'ethers', // Map ethers5 to ethers
+      ethers5: 'ethers',
     },
   },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][ext]'
+      }
+    }
+  },
+  publicDir: 'public',
 })
